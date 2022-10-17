3beb58809ac890fc3927379d77661bea31201d99
Crowdbotics <engineering@crowdbotics.com>
2022-10-17 11:22:46 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/qwertyuj_dev_39057/MainActivity.java
@@ -0,0 +1,15 @@
+package com.qwertyuj_dev_39057;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "qwertyuj_dev_39057";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "qwertyuj_dev_39057",
+  url: "https://qwertyuj_dev_39057.botics.co",
+  api: "https://qwertyuj_dev_39057.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
